7ecdda1f4d019c04caabd8cc53e570b208e38f70
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-06 21:02:37 +0900

    add

--- test.rb
+++ test.rb
@@ -1,14 +1,3 @@
-all = gets.to_i
-mon = gets.to_i
-tus = gets.to_i
-wed = gets.to_i
-thu = gets.to_i
-fri = gets.to_i
-sat = gets.to_i
-sun = gets.to_i
-num = mon+tus+wed+thu+fri+sat+sun
-if num <= all then
-  puts num
-else
-  puts all
-end
+d = gets.to_i
+total = d/180+2
+puts total
