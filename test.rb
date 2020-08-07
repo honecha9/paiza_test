39cb958a1395980dd6c014638b5fcdd1e14bacbc
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-07 14:25:15 +0900

    add

--- test.rb
+++ test.rb
@@ -1,4 +1,16 @@
-n = gets.to_i
-m = gets.to_i
-num = n*n-m
-puts num
+a,b = gets.split.map(&:to_i)
+num = gets.to_i
+num.times do
+  s = gets.chomp.split(" ")
+  if s[0].to_i< a then
+    puts "High"
+  elsif s[0].to_i == a
+    if s[1].to_i < b then
+      puts "Low"
+    else 
+      puts "High"
+    end
+  else
+    puts "Low"
+  end
+end
