e146024e96eb3914054fb988f0e839b5eddeccc2
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-07 15:12:12 +0900

    add

--- test.rb
+++ test.rb
@@ -1,16 +1,8 @@
 a,b = gets.split.map(&:to_i)
-num = gets.to_i
-num.times do
-  s = gets.chomp.split(" ")
-  if s[0].to_i< a then
-    puts "High"
-  elsif s[0].to_i == a
-    if s[1].to_i < b then
-      puts "Low"
-    else 
-      puts "High"
-    end
-  else
-    puts "Low"
-  end
+a.times do
+  i = gets.to_i
+  qi = i-1
+  g = b.to_s(2)
+  gb = g.reverse
+  puts  gb[qi]
 end
