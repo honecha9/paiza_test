62bf58df493a65a66472173de28f498526031568
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-09 11:17:57 +0900

    add

--- test.rb
+++ test.rb
@@ -1,8 +1,6 @@
-num = gets.to_i
-a = 0
-array = []
-while a <= num do
-  array << a
-  a += 1
-end
-puts array.sum
+gold = gets.chomp
+silver = gets.chomp
+bronse = gets.chomp
+puts "Gold #{gold}"
+puts "Silver #{silver}"
+puts "Bronze #{bronse}"
