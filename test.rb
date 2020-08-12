bbb1246579ad69e46191d1e2058b9cdf262b1338
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-12 16:25:46 +0900

    add

--- test.rb
+++ test.rb
@@ -1,14 +1,3 @@
-a,b,c = gets.split.map(&:to_i)
-num = gets.to_i
-num.times do
-  s = gets.split(' ')
-  s1 = s[1].to_i
-  s2 = s[2].to_i
-  s3 = s[3].to_i
-  s4 = s[4].to_i
-  s5 = s[5].to_i
-  s6 = s[6].to_i
-    if a.between?(s1, s2) && b.between?(s3, s4) && c.between?(s5, s6) then
-      puts s[0]
-    end
-end
+n = gets.to_i
+m = gets.chomp
+puts m*n
