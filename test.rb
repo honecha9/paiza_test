53343da78d19a5cd06519537adc1f680e789409d
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-15 18:30:43 +0900

    add

--- test.rb
+++ test.rb
@@ -1,8 +1,8 @@
-a = gets.to_i
-if a <= 30 then
-  puts "sunny"
-elsif a.between?(31, 70) then
-  puts "cloudy"
-else  
-  puts  "rainy"
-end
+m, b, c = gets.split.map(&:to_i)
+num = m-(m*(0.01*b))
+num1 = num.round(1)
+puts num1
+ww = num1*(0.01*c)
+www = ww.round(2)
+aaa = num1 - www
+puts aaa.round(2)
