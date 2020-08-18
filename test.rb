c737fe924f87bdbc8bd12d22c5125b09addd4cd0
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-18 23:53:07 +0900

    add

--- test.rb
+++ test.rb
@@ -1,4 +1,12 @@
-num = gets.to_i
-num = num-1
-line = gets.split(' ')
-puts line[num]
+a = gets.to_i
+if a == 5 then
+  puts "A"
+elsif a == 3 then
+  puts "C"
+elsif a == 2 then
+  puts "D"
+elsif a == 4 then
+  puts "B"
+else  
+  puts "E"
+end
