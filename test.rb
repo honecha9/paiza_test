a8e0e889d419e9811c774428270f7ece7f7ab9bb
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-18 23:56:51 +0900

    add

--- test.rb
+++ test.rb
@@ -1,12 +1,3 @@
 a = gets.to_i
-if a == 5 then
-  puts "A"
-elsif a == 3 then
-  puts "C"
-elsif a == 2 then
-  puts "D"
-elsif a == 4 then
-  puts "B"
-else  
-  puts "E"
-end
+num = 6*a**2
+puts num
