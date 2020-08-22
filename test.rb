c1ef10b748422b811e1f55b32e5a36d67faf7d77
honecha9 <65473048+honecha9@users.noreply.github.com>
2020-08-22 14:13:28 +0900

    add

--- test.rb
+++ test.rb
@@ -1,19 +1,2 @@
-aa, bb = gets.split.map(&:to_i)
-if aa >= 5 then
-  if bb >= 5
-    puts 10
-  else
-    num = 5+bb
-    puts num
-  end
-elsif bb >= 5 then
-  if aa >= 5
-    puts 10
-  else  
-    num = 5+aa
-    puts num
-  end
-else  
-  num = aa+ bb 
-  puts num
-end
+moji = gets.chomp
+puts moji.count('A')
